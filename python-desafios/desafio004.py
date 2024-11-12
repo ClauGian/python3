40d2d31b0aca85732a6875cf94a73d63ca2bd1fa
ClauGian <cgclaugian@gmail.com>
2024-11-11 22:04:15 -0300

    exercicios

---
+++ python-desafios/desafio004.py
@@ -0,0 +1,15 @@
+n = str(input('Digite algo: '))
+print(n)
+print(n.isalnum())
+print(n.isalpha())
+print(n.isascii())
+print(n.isdigit())
+print(n.islower())
+print(n.isdecimal())
+print(n.isidentifier())
+print(n.isnumeric())
+print(n.isprintable())
+print(n.isspace())
+print(n.istitle())
+
+
