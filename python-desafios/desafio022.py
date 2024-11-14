cc7954810a3f6789a1f49ca3d1dacfa93d5592b3
ClauGian <cgclaugian@gmail.com>
2024-11-14 18:31:41 -0300

    correção execícios

--- python-desafios/desafio022.py
+++ python-desafios/desafio022.py
@@ -1,12 +1,12 @@
-nome = 'Claudinei Gianini'
-print('Nome: ',nome)
-print(nome.upper())
-print(nome.lower())
-print(len(nome))
-print(len(nome.split()[0] + nome.split()[1]))
-print(nome.split()[0])
-print(len(nome.split()[0]))
-print(nome.split()[1])
-print(len(nome.split()[1]))
+nome = str(input('Digite seu nome completo: '))
+print('Seu nome é: ',nome)
+print('Seu nome em letras maiusculas: {}.'.format(nome.upper()))
+print('Seu nome em letras minusculas: {}.'.format(nome.lower()))
+print('Seu nome com espaços possui {} caracteres.'.format(len(nome)))
+print('Seu nome sem espaços possui {} caracteres.'.format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome é: {}.'.format(nome.split()[0]))
+print('Seu primeiro nome possui {} caracteres.'.format(len(nome.split()[0])))
+print('Seu sobrenome é: {}.'.format(nome.split()[-1]))
+print('Seu sobrenome possui {} caracteres.'.format(len(nome.split()[-1])))
 
 
--- python-desafios/desafio023.py
+++ python-desafios/desafio023.py
@@ -1,8 +1,8 @@
-num = input('Escreva um número entre 0 e 9999: ')
-uni = num[3]
-dez = num[2]
-cen = num[1]
-mil = num[0]
+num = int(input('Escreva um número entre 0 e 9999: '))
+uni = num // 1 % 10
+dez = num // 10 % 10
+cen = num // 100 % 10
+mil = num // 1000 % 10
 print('Você digitou o número: {}'.format(num))
 print('A sua unidade é: {}'.format(uni))
 print('A sua dezena  é: {}'.format(dez))
