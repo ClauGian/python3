5c3576965b4bb729eb18efa1ffdda974e3ffc4e4
ClauGian <cgclaugian@gmail.com>
2024-11-24 08:18:39 -0300

    exercicio

--- python-desafios/desafio067.py
+++ python-desafios/desafio067.py
@@ -1,7 +1,12 @@
-# NÃO CHEGUEI A UM CONSENSO.
 c = 0
-n = int(input('Quer ver a tabuada de que número? '))
-while c <= 10:
+while True:
+    n = int(input('Quer ver a tabuada de que número? '))
+    print('-.' * 20)
+    if n < 0:
+        break
+    for c in range(1, 11):
+        print(f'{n}  x  {c}  =  {n * c}')
+    print('-.' * 20)
+print('PROGRAMA TABUADA ENCERRADO! Volte Sempre!')
 
-    print(f'{n}  x  {c}  =  {n * c}')
-    c += 1
+
